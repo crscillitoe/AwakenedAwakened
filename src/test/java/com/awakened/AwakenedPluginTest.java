f7168e8d58464111ceb7fe63cd46beeeb79459e9
crscillitoe <christian.scillitoe@gmail.com>
2026-03-24 09:59:20 -0700

    Update plugin from example plugin

--- src/test/java/com/awakened/AwakenedPluginTest.java
+++ src/test/java/com/awakened/AwakenedPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.awakened;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class AwakenedPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(AwakenedPlugin.class);
 		RuneLite.main(args);
 	}
 }
